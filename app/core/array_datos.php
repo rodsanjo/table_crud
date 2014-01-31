709cf2b3280e4770eaafb715b3892ef7771f391a
Jorge Rodríguez Sanz <jergo23@gmail.com>
2014-01-31 14:55:01 +0100

    Revert "Mal"
    This reverts commit 38ff1b12b29d902e42c9318e56dc06651db72dff.

---
+++ app/core/array_datos.php
@@ -0,0 +1,11 @@
+<?php
+namespace core;
+
+/**
+ * Clase con métodos útiles para extraer información del arrays.
+ * Todos los métodos devuelven el valor asociado a la entrada del array
+ * valor o null en caso de que no exista la entrada.
+ */
+class Array_Datos extends \core\Datos {
+		
+}
